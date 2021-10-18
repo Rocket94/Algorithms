07b1da02b7ebbb24dc3b90eb3b6ebf572f86016e
Mario Peng (彭荣坤) <pengrongkun@inspur.com>
2021-10-18 16:07:42 +0800

    b_search ++

--- algorithms/search_test.go
+++ algorithms/search_test.go
@@ -17,8 +17,45 @@ func TestBsearch1(t *testing.T) {
 	createBserchTest1(t, []int{0, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 0, 0)
 	createBserchTest1(t, []int{1, 1, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 1, 0)
 	createBserchTest1(t, []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1, 0)
-	createBserchTest(t, []int{0}, 1, -1)
-	createBserchTest(t, nil, 1, -1)
+	createBserchTest1(t, []int{0}, 1, -1)
+	createBserchTest1(t, nil, 1, -1)
+}
+
+func TestBsearch2(t *testing.T) {
+	createBserchTest2(t, []int{1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 8, 7)
+	createBserchTest2(t, []int{0, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 0, 0)
+	createBserchTest2(t, []int{1, 1, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 1, 2)
+	createBserchTest2(t, []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1, 11)
+	createBserchTest2(t, []int{1, 2, 3, 3, 3, 4, 5, 6, 7, 7, 8, 8}, 8, 11)
+	createBserchTest2(t, []int{0}, 1, -1)
+	createBserchTest2(t, nil, 1, -1)
+}
+
+func TestBsearch3(t *testing.T) {
+	createBserchTest3(t, []int{1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 8, 5)
+	createBserchTest3(t, []int{1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 10, 8)
+	createBserchTest3(t, []int{0, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 0, 0)
+	createBserchTest3(t, []int{1, 1, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 1, 0)
+	createBserchTest3(t, []int{1, 1, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 18, 11)
+	createBserchTest3(t, []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1, 0)
+	createBserchTest3(t, []int{1, 2, 3, 3, 3, 4, 5, 6, 7, 7, 8, 8}, 18, -1)
+	createBserchTest3(t, []int{0}, -1, 0)
+	createBserchTest3(t, []int{0}, 1, -1)
+	createBserchTest3(t, nil, 1, -1)
+}
+
+func TestBsearch4(t *testing.T) {
+	createBserchTest4(t, []int{1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 8, 7)
+	createBserchTest4(t, []int{1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 10, 7)
+	createBserchTest4(t, []int{0, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 0, 0)
+	createBserchTest4(t, []int{1, 1, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 1, 2)
+	createBserchTest4(t, []int{1, 1, 1, 3, 4, 5, 6, 8, 8, 8, 11, 18}, 18, 11)
+	createBserchTest4(t, []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1, 11)
+	createBserchTest4(t, []int{1, 2, 3, 3, 3, 4, 5, 6, 7, 7, 8, 8}, 18, 11)
+	createBserchTest4(t, []int{1, 2, 3, 3, 3, 4, 5, 6, 7, 7, 8, 8}, 0, -1)
+	createBserchTest4(t, []int{0}, -1, -1)
+	createBserchTest4(t, []int{0}, 1, 0)
+	createBserchTest4(t, nil, 1, -1)
 }
 
 func createBserchTest(t *testing.T, arr []int, val int, res int) {
@@ -30,6 +67,27 @@ func createBserchTest(t *testing.T, arr []int, val int, res int) {
 
 func createBserchTest1(t *testing.T, arr []int, val int, res int) {
 	actual := Bsearch1(arr, val)
+	if res != actual {
+		t.Errorf("expect:%v,but found %v", res, actual)
+	}
+}
+
+func createBserchTest2(t *testing.T, arr []int, val int, res int) {
+	actual := Bsearch2(arr, val)
+	if res != actual {
+		t.Errorf("expect:%v,but found %v", res, actual)
+	}
+}
+
+func createBserchTest3(t *testing.T, arr []int, val int, res int) {
+	actual := Bsearch3(arr, val)
+	if res != actual {
+		t.Errorf("expect:%v,but found %v", res, actual)
+	}
+}
+
+func createBserchTest4(t *testing.T, arr []int, val int, res int) {
+	actual := Bsearch4(arr, val)
 	if res != actual {
 		t.Errorf("expect:%v,but found %v", res, actual)
 	}
--- algorithms/serch.go
+++ algorithms/serch.go
@@ -65,15 +65,59 @@ func Bsearch1(arr []int, val int) int {
 
 //变体二：查找最后一个值等于给定值的元素
 func Bsearch2(arr []int, val int) int {
+	high := len(arr) - 1
+	low := 0
+	for low <= high {
+		mid := low + (high-low)>>1
+		if arr[mid] > val {
+			high = mid - 1
+		} else if arr[mid] < val {
+			low = mid + 1
+		} else {
+			if mid == len(arr)-1 || arr[mid+1] != val {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		}
+	}
 	return -1
 }
 
 //变体三：查找第一个大于等于给定值的元素
 func Bsearch3(arr []int, val int) int {
+	high := len(arr) - 1
+	low := 0
+	for low <= high {
+		mid := low + (high-low)>>1
+		if arr[mid] >= val {
+			if mid == 0 || arr[mid-1] < val {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		} else {
+			low = mid + 1
+		}
+	}
 	return -1
 }
 
 //变体四：查找最后一个小于等于给定值的元素
 func Bsearch4(arr []int, val int) int {
+	high := len(arr) - 1
+	low := 0
+	for low <= high {
+		mid := low + (high-low)>>1
+		if arr[mid] <= val {
+			if mid == len(arr)-1 || arr[mid+1] > val {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		} else {
+			high = mid - 1
+		}
+	}
 	return -1
 }
